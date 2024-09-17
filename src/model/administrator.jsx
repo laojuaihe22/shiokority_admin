cb2de960ae1e1f406da3d8e33e907a6e8c70b2a2
laojuaihe22 <jylee073@mymail.sim.edu.sg>
2024-09-17 16:54:44 +0800

    for deployment

---
+++ src/model/administrator.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+class Administrator {
+  
+  static async login(data) {
+    try {
+      const response = await axios.post(`/login/admin`, data);      
+      localStorage.setItem('token', response.data.token);
+      return response.data;
+    } catch (error) {
+      throw new Error('Wrong email or password');
+    }
+  }
+
+  static async logout() {
+    try {
+      await axios.post('/logout/admin'); // Assuming your API has a logout endpoint
+    } catch (error) {
+      console.error('Logout failed:', error);
+    } finally {
+      localStorage.removeItem('token');
+    }
+  }
+
+  static isLoggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  
+}
+
+export default Administrator;
